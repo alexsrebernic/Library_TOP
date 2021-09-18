94fed7022a46a8261aba267247abf22fd561dbaf
Alex Srebernic <alexsrebernic@gmail.com>
2021-09-18 18:45:52 -0300

    add card image,add delete,add card to catalog function, and remove,add to mylibrary function

--- app.js
+++ app.js
@@ -1,49 +1,134 @@
 const logInButton = document.querySelector("#logIn")
 const addBookButton = document.querySelector("#addBook")
-const catalogDisplay = document.querySelector("#catalog")
 
+const catalogDisplay = document.querySelector("#catalog")
 const backgroundPopUpElement = document.querySelector("#backgroundPopUp")
 const closePopUpElement = document.querySelector("#close")
 
+const titleInput = document.querySelector("#titleInput")
+const authorInput = document.querySelector("#authorInput")
+const pagesInput = document.querySelector("#pagesInput")
+const urlInput = document.querySelector("#urlInput")
+const readBoolean = document.querySelector("#readCheck")
+const submitButton = document.querySelector("#submit")
+
+let myLibrary = []
+myLibrary.forEach(book => {
+    book.onclick = () => console.log("asd")
+})
 addBookButton.onclick = () => displayPopUp();
 closePopUpElement.onclick = () => closePopUp();
+submitButton.onclick = () => submitForm()
+
+document.addEventListener('click',function(e){
+    let element = document.getElementById(e.target.id)
+    if(element.textContent === "Read" || element.textContent === "Not Read"){
+        checkRead(element)
+    }
+    if(element.textContent === "Delete"){
+        deleteCard(element)
+    }
+})
 
 function displayPopUp (){
-backgroundPopUpElement.style.display = "flex"
-backgroundPopUpElement.style.transform = "scale(1)"
-
+    backgroundPopUpElement.style.display = "flex"
+    backgroundPopUpElement.style.transform = "scale(1)"
 }
 function closePopUp (){
-backgroundPopUpElement.style.display = "none"
-
+    backgroundPopUpElement.style.display = "none"
 }
 
+function checkRead(readCard){
+    let parent = readCard.parentNode
+    let index = Array.from(parent.parentNode.children).indexOf(parent)
+    if(readCard.textContent == "Read"){
+        readCard.setAttribute("class","readCardFalse")
+        readCard.textContent = "Not Read"
+        myLibrary[index].read = false
+    } else if(readCard.textContent == "Not Read"){
+        readCard.setAttribute("class","readCardTrue")
+        readCard.textContent = "Read"
+        myLibrary[index].read = true
+    }
+}
 
-function Book(title,author,pages,read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    let readFalseOrTrue = (read) => {
-        if(read){
-            return "Already read!"
+function deleteCard(element){
+    let parent = element.parentNode
+    let index = Array.from(parent.parentNode.children).indexOf(parent)
+    catalogDisplay.removeChild(parent)
+    myLibrary.splice(index,1)
+    parent.classList.add("desvanecerse")
+    console.log(myLibrary)
+}
 
+function submitForm (){
+    if(titleInput.value == "" || authorInput.value == "" || pagesInput.value == "") return
+    closePopUp()
+    event.preventDefault()
+    let book = new Book(titleInput.value,authorInput.value,pagesInput.value,readBoolean.value)
+    addBookToLibrary(book)
+    createCard()
+    clearForm()
+    console.log(myLibrary)
+}
+function createCard(){
+    const card = document.createElement("div")
+    const titleText= document.createElement("h2")
+    const imgUrl = document.createElement("img")
+    const authorText = document.createElement("h2")
+    const pagesText = document.createElement("h2")
+    const readCard = document.createElement("button")
+    const deleteCard = document.createElement("button")
+    deleteCard.setAttribute("class","deleteCard")
+    readCard.setAttribute("class","readCard")
+    readCard.id = Math.floor(Math.random() * 100)
+    deleteCard.id = Math.floor(Math.random() * 100)
+    titleText.id = "titleText"
+    imgUrl.id = "imgUrl"
+    card.appendChild(titleText)
+    card.appendChild(imgUrl)
+    card.appendChild(authorText)
+    card.appendChild(pagesText)
+    card.appendChild(readCard)
+    card.appendChild(deleteCard)
+    card.setAttribute("class","card")
+    titleText.textContent = titleInput.value
+    authorText.textContent = "By: " + authorInput.value
+    pagesText.textContent = pagesInput.value + " pages"
+    deleteCard.textContent = "Delete"
+    if(readBoolean.checked){
+        readCard.setAttribute("class","readCardTrue")
+        readCard.textContent = "Read"
     } else {
-        return "Not read yet"
+        readCard.setAttribute("class","readCardFalse")
+        readCard.textContent = "Not Read"
     }
-}
-    this.info = function(){
-        return this.title + " by " + this.author  + "," + this.pages + " pages, " + readFalseOrTrue(this.read)
+    if(urlInput.value == ""){
+        imgUrl.src = "img/not-available.jpg"
+    } else{
+        imgUrl.src = urlInput.value 
     }
+    catalogDisplay.appendChild(card)
 }
-let myLibrary = []
-function addBookToLibrary(){
-    if(new Book() instanceof Book){
-        myLibrary.push(new Book)
-    }
-}
-function displayBooks(){
-    for(let l = 0, n = myLibrary.length; l < n ; l++){
 
-    } 
+function clearForm(){
+    titleInput.value = ""
+    authorInput.value = ""
+    pagesInput.value = ""
+    urlInput.value = ""
+}
+
+
+class Book{
+        constructor(title,author,pages,read){
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read            
+    }
+}
+function addBookToLibrary(newBook){
+    if(newBook instanceof Book){
+        myLibrary.push(newBook)
+    }
 }
